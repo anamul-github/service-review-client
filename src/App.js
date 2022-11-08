48522917a7db9866e6fa46ed3ee24da2662745ec
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-07 21:28:10 -0500

    added some components and set up router with some routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Footer from './components/Shared/Footer/Footer';
+import Main from './layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: '/footer',
+          element: <Footer></Footer>
+        }
+      ]
+    }
+  ])
+
   return (
     <div>
-      <h3>Let's Check</h3>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center bg-dark text-white p-4'>
+            <h2>This is Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
