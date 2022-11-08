5b016e07cb17359533a3438c950198a719fd99b0
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-07 22:45:12 -0500

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <img
+                className=" w-100"
+                src="https://www.muscleandfitness.com/wp-content/uploads/2015/07/CubeCover.jpg?quality=86&strip=all"
+                alt="banner"
+            />
         </div>
     );
 };
